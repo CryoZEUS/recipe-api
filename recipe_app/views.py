4688376ca125ac8456b060c80e687d3d9326f169
cryozeus <yashbhardwaj911@gmail.com>
2021-04-09 20:35:07 +0530

    refactored templates, statics

---
+++ recipe_app/views.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+
+from . import app
+
+
+@app.route("/")
+def home():
+    """Render Home Page"""
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    """Render About Page"""
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    """Render Contact Page"""
+    return render_template("contact.html")
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    """
+    Render hello_there Page
+    args : name
+    """
+    return render_template(
+        "hello_there.html",
+        name = name,form=date = datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
